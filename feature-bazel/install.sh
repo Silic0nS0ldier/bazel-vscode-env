99c4622e4d9879987929ba3917b956d135e55e6c
Jordan Mele <jordan.mele@outlook.com.au>
2025-07-12 04:35:45 +0000

    Set local Java version (21) and add renovate config

--- feature-bazel/install.sh
+++ feature-bazel/install.sh
@@ -6,19 +6,26 @@ echo "Running install..."
 
 ARCH="$(uname --processor)"
 
+# renovate: datasource=github-tags depName=bazelbuild/bazelisk
+BAZELISK_VERSION="v1.26.0"
+# renovate: datasource=github-tags depName=bazelbuild/buildtools
+BUILDTOOLS_VERSION="v8.2.1"
+# renovate: datasource=github-tags depName=withered-magic/starpls
+STARPLS_VERSION="v0.1.21"
+
 if [[ $ARCH == "arm64" ]] || [[ $ARCH == "aarch64" ]]; then
-    curl https://github.com/bazelbuild/bazelisk/releases/download/v1.26.0/bazelisk-linux-arm64 -Lo /usr/local/bin/bazel
-    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/buildifier-linux-arm64 -Lo /usr/local/bin/buildifier
-    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/buildozer-linux-arm64 -Lo /usr/local/bin/buildozer
-    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/unused_deps-linux-arm64 -Lo /usr/local/bin/unused_deps
-    curl https://github.com/withered-magic/starpls/releases/download/v0.1.21/starpls-linux-aarch64 -Lo /usr/local/bin/starpls
+    curl "https://github.com/bazelbuild/bazelisk/releases/download/${BAZELISK_VERSION}/bazelisk-linux-arm64" -Lo /usr/local/bin/bazel
+    curl "https://github.com/bazelbuild/buildtools/releases/download/${BUILDTOOLS_VERSION}/buildifier-linux-arm64" -Lo /usr/local/bin/buildifier
+    curl "https://github.com/bazelbuild/buildtools/releases/download/${BUILDTOOLS_VERSION}/buildozer-linux-arm64" -Lo /usr/local/bin/buildozer
+    curl "https://github.com/bazelbuild/buildtools/releases/download/${BUILDTOOLS_VERSION}/unused_deps-linux-arm64" -Lo /usr/local/bin/unused_deps
+    curl "https://github.com/withered-magic/starpls/releases/download/${STARPLS_VERSION}/starpls-linux-aarch64" -Lo /usr/local/bin/starpls
 
 elif [[ $ARCH == "x86_64" ]]; then
-    curl https://github.com/bazelbuild/bazelisk/releases/download/v1.26.0/bazelisk-linux-amd64 -Lo /usr/local/bin/bazel
-    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/buildifier-linux-amd64 -Lo /usr/local/bin/buildifier
-    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/buildozer-linux-amd64 -Lo /usr/local/bin/buildozer
-    curl https://github.com/bazelbuild/buildtools/releases/download/v8.2.1/unused_deps-linux-amd64 -Lo /usr/local/bin/unused_deps
-    curl https://github.com/withered-magic/starpls/releases/download/v0.1.21/starpls-linux-amd64 -Lo /usr/local/bin/starpls
+    curl "https://github.com/bazelbuild/bazelisk/releases/download/${BAZELISK_VERSION}/bazelisk-linux-amd64" -Lo /usr/local/bin/bazel
+    curl "https://github.com/bazelbuild/buildtools/releases/download/${BUILDTOOLS_VERSION}/buildifier-linux-amd64" -Lo /usr/local/bin/buildifier
+    curl "https://github.com/bazelbuild/buildtools/releases/download/${BUILDTOOLS_VERSION}/buildozer-linux-amd64" -Lo /usr/local/bin/buildozer
+    curl "https://github.com/bazelbuild/buildtools/releases/download/${BUILDTOOLS_VERSION}/unused_deps-linux-amd64" -Lo /usr/local/bin/unused_deps
+    curl "https://github.com/withered-magic/starpls/releases/download/${STARPLS_VERSION}/starpls-linux-amd64" -Lo /usr/local/bin/starpls
 else
     echo "Unknown arch $ARCH"
     exit 1
@@ -33,4 +40,3 @@ chmod +x \
 # 'ed' used in src/test/shell/bazel/starlark_repository_test.sh@test_disallow_unverified_http
 sudo apt update  # Populate package metadata store
 sudo apt install ed
-
